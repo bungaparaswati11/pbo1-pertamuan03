41f49e2c79ac1d0d4214107fc670acdaf50fe850
bungaparaswati11 <bungaparaswati11@gmail.com>
2020-04-11 18:46:44 -0700

    Percakapan switch

---
+++ src/percabangan/switchke2.java
@@ -0,0 +1,29 @@
+package percabangan;
+
+public class switchke2 {
+    public static void main(String[] args) {
+        char nilai = 'D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs   : \"Terimakasih pak\"");
+                System.out.println("Dosen : \"Selamat yaa !\"");
+                break;
+            case 'B':
+                System.out.println("Mhs   : \"Kenapa ulun kada dapat A , pak ?:)\"");
+                System.out.println("Dosen : \"!@#$%\"");
+                break;
+            case 'C':
+                System.out.println("Mhs   : \"Ulun turun pul Pak ai, tugas pul jua \"");
+                System.out.println("Dosen : \"tapi bisalah menjawab ujian?\"");
+                System.out.println("Mhs   : \"hehehehehhehehee\"");
+                break;
+            default:
+                System.out.println("Mhs   : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen : \"hmm , Bujur jua kah ?\"");
+                System.out.println("Mhs   : \"memeriksa berkass\"");
+                System.out.println("Dosen : \"menceleng\"");
+                System.out.println("Mhs   : \"Kaburrrr...\"");
+            break;
+        }
+    }
+}
